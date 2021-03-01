681ba4017803d9722df8ac48499ce132414f352e
Naomi <naomilang1212@gmail.com>
2021-02-28 19:16:42 -0800

    basic routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+  addComment,
+  removeComment,
+  addReply,
+  removeReply
+} = require('../../controllers/comment-controller');
+
+// /api/comments/<userId>
+router.route('/:userId').post(addComment);
+
+// /api/comments/<pizzaId>/<commentId>
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
+
+// /api/comments/<pizzaId>/<commentId>/<replyId>
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
+module.exports = router;
